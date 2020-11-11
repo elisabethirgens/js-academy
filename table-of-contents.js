ac238130ceea67047115f14472abcd42c2824d10
Elisabeth Irgens <hello@elisabethirgens.com>
2020-11-11 20:03:01 +0100

    Generate links also to headings without IDs

--- table-of-contents.js
+++ table-of-contents.js
@@ -1,11 +1,16 @@
 const toc = document.querySelector("#table-of-contents");
 let headings = document.querySelectorAll("h2");
+let anchorReplacement = 0;
 
 function renderTable() {
   if (headings.length) {
     headings = Array.prototype.slice
       .call(headings)
       .map(function (item) {
+        if (!item.hasAttribute("id")) {
+          anchorReplacement++;
+          item.setAttribute("id", anchorReplacement);
+        }
         const anchor = item.getAttribute("id");
         const heading = item.textContent;
         return `<li><a href="#${anchor}">${heading}</a></li>`;
