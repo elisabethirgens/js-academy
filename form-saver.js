e69cc541b41d2405d78472f249fb5a5f4bf8ec4d
Elisabeth Irgens <hello@elisabethirgens.com>
2020-12-13 13:06:39 +0100

    Add project autosave for other field types
    Radio buttons, checkboxes and select means the script needs to be
    modified to handle storing and loading values from those fields too.
    
    This commit is a bit clunky, but it's the result of my first working
    solution before looking at the project review. Refactoring up next!

--- form-saver.js
+++ form-saver.js
@@ -1,47 +1,77 @@
 const storageID = "form-autosave";
 
 function getElemId(field) {
-  if (field.id) {
-    return field.id;
-  }
+  // Prefer name, so unique id can be used for labels
   if (field.name) {
     return field.name;
+  }
+  if (field.id) {
+    return field.id;
   }
   return null;
 }
 
-function storeData(event) {
+function inputHandler(event) {
   if (!event.target.closest("#save-me")) return;
   const id = getElemId(event.target);
   if (!id) return;
+  let relevantValue = event.target.value;
+  if (event.target.type === "checkbox") {
+    if (!event.target.hasAttribute("checked")) {
+      event.target.setAttribute("checked", "");
+    } else {
+      event.target.removeAttribute("checked", "");
+    }
+    relevantValue = event.target.checked;
+  }
+  storeData(id, relevantValue);
+}
+
+function storeData(id, value) {
   // Create object and get any existing data before adding new values
   let dataObj = localStorage.getItem(storageID);
   dataObj = dataObj ? JSON.parse(dataObj) : {};
-  dataObj[id] = event.target.value;
+  dataObj[id] = value;
   localStorage.setItem(storageID, JSON.stringify(dataObj));
 }
 
-function repopulateForm() {
+function loadData() {
   let savedData = localStorage.getItem(storageID);
   if (!savedData) return;
   savedData = JSON.parse(savedData);
   const fieldsAll = Array.prototype.slice.call(
-    document.querySelectorAll("#save-me input, #save-me textarea")
+    document.querySelectorAll(
+      "#save-me input, #save-me select, #save-me textarea"
+    )
   );
   fieldsAll.forEach(function (field) {
     const id = getElemId(field);
     if (!id) return;
     const saved = savedData[id];
+    if (field.type === "checkbox") {
+      if (saved === true) {
+        field.setAttribute("checked", "");
+      } else {
+        field.removeAttribute("checked", "");
+      }
+      return; // to avoid setting DOM value to 'on' for checkboxes
+    }
     if (!saved) return;
-    field.value = saved;
+    if (field.type === "radio") {
+      if (field.value === saved) {
+        field.setAttribute("checked", "");
+      }
+      return; // to avoid changing DOM value to saved value for both radio buttons
+    }
+    field.value = saved; // for input text and select only
   });
 }
-repopulateForm();
+loadData();
 
 function clearForm(event) {
   if (event.target.id !== "save-me") return;
   localStorage.removeItem(storageID);
 }
 
-document.addEventListener("input", storeData);
+document.addEventListener("input", inputHandler);
 document.addEventListener("submit", clearForm);
